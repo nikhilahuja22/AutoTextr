28e3c4f1eac491de5a6bbbb7a874de8c2a196add
sramini <sramini@calpoly.edu>
2017-09-16 17:14:21 -0700

    updated working app.js

--- app.js
+++ app.js
@@ -9,38 +9,63 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+//
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+//
+// // uncomment after placing your favicon in /public
+// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+// app.use('/', index);
+// app.use('/users', users);
+//
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.post('/inbound', (req, res) => {
+    handleParams(req.body, res);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+function handleParams(params, res) {
+    if (!params.to || !params.msisdn) {
+        console.log('This is not a valid inbound SMS message!');
+    } else {
+        console.log('Success');
+        let incomingData = {
+            messageId: params.messageId,
+            from: params.msisdn,
+            text: params.text,
+            type: params.type,
+            timestamp: params['message-timestamp']
+        };
+        res.send(incomingData);
+    }
+    res.status(200).end();
+}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+const server = app.listen(3000, () => {
+        console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
 
 module.exports = app;
