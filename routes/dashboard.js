d99baae0e6203bdeedcbe1ce4e40033993448cc8
Nikhil Ahuja <nikhilahuja@Nikhils-MacBook-Pro.local>
2017-11-11 13:40:28 -0800

    User list populated with ejs

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose')
+
+let Order = require('../app/models/order');
+
+mongoose.connect('mongodb://localhost:27017/users');
+let db = mongoose.connection;
+
+
+db.once('open', function(){
+  console.log('Connection to mongodb');
+})
+
+db.on('error', function(err){
+  console.log(err);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard', { title: 'Express' });
+	Order.find({}, function(err, orders) {
+		if (err) {
+			console.log(err);
+		}
+	  res.render('dashboard', { 
+	  	title: 'Express',
+	  	orders: orders
+	   });		
+	});
+
+
 });
 
 module.exports = router;
